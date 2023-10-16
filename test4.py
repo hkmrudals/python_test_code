8a72c189aca6b1320b66e3da6c929ec9c9ea667a
hkmrudals <148044084+hkmrudals@users.noreply.github.com>
2023-10-16 16:39:18 +0900

    Update test4.py

--- test4.py
+++ test4.py
@@ -14,6 +14,6 @@ print("나는 {1}색과 {0}색을 좋아해요".format("파란", "빨간"))
 
 print("나는 {age}살이며 {color}색을 좋아해요".format(age = 20, color = "빨간"))
 
-age = 20
+age = 10
 color = "빨간"
 print(f"나는 {age}살이며 {color}색을 좋아해요.")
